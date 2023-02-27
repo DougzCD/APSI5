3226d5adeed8f14ba310747d514b0dac653ca2fe
DougzCD <110394134+DougzCD@users.noreply.github.com>
2023-02-27 14:01:50 +0000

    Aula 27022023

---
+++ NomedaClasse.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+class NomeDaClasse {
+    public:
+        float atrPublico[100] ;
+
+        static bool atrEstatico ;
+
+       
+
+    
+    protected:
+        std::string atrProtegido;
+    private:
+        int atrPrivado;
+    
+    
+   
+    metodo1(float);
+    metodo2('float'){float}
+    metodoAbstrato(){bool}
+    metodoEstatico(){ClassB}
+
+   
+}
